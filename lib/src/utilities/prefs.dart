a5c9f37838777a802f1ffed78d01b93ef0665e0a
Hanzla <mhanzla80@gmail.com>
2023-02-23 20:05:43 +0500

    fix. prefs initialization.

--- lib/src/utilities/prefs.dart
+++ lib/src/utilities/prefs.dart
@@ -5,18 +5,15 @@ import 'package:shared_preferences/shared_preferences.dart';
 class Prefs {
   static final Prefs _instance = Prefs._internal();
   static Prefs get instance => _instance;
-
-  factory Prefs() {
-    return _instance;
-  }
+  factory Prefs() => _instance;
   Prefs._internal();
 
-  static late SharedPreferences _prefs;
+  static SharedPreferences? _prefs;
 
   static const _keyFavWalls = "FAV_WALLS";
   static const _keyAllWalls = "ALL_WALLS";
 
-  Future<void> init() async => _prefs = await SharedPreferences.getInstance();
+  Future<void> init() async => _prefs ??= await SharedPreferences.getInstance();
 
   Future<bool> toggleFavWallpaper(String favWall) {
     final favList = getFavWallpapers();
@@ -25,16 +22,16 @@ class Prefs {
     } else {
       favList.add(favWall);
     }
-    return _prefs.setStringList(_keyFavWalls, favList);
+    return _prefs?.setStringList(_keyFavWalls, favList) ?? Future.value(false);
   }
 
-  List<String> getFavWallpapers() => _prefs.getStringList(_keyFavWalls) ?? [];
+  List<String> getFavWallpapers() => _prefs?.getStringList(_keyFavWalls) ?? [];
 
   void setWallpapersData(Map<String, dynamic> wallpapersData) =>
-      _prefs.setString(_keyAllWalls, json.encode(wallpapersData));
+      _prefs?.setString(_keyAllWalls, json.encode(wallpapersData));
 
   Map<String, dynamic>? getWallpapersData() {
-    final prefWalls = _prefs.getString(_keyAllWalls);
+    final prefWalls = _prefs?.getString(_keyAllWalls);
     if (prefWalls == null) return null;
     return jsonDecode(prefWalls);
   }
